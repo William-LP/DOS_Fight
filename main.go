c5d0b0b043e4360e572b0d562607949d9adcab1b
William-LP <william.lepommelet@gmail.com>
2019-06-22 01:08:48 +0200

    build a hero in log

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/William-LP/go_training/pkg/mario"
+)
+
+func main() {
+	h := mario.CreateHero(10, 10, 10, 10, 10)
+	log.Println(h)
 }
