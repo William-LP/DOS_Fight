81886ce1bc422dd73dbe5dacb1202261eda6a1f2
Will <william.lepommelet@gmail.com>
2020-03-22 23:05:15 -0300

    exclude .exe in gitignore

--- main.go
+++ main.go
@@ -3,30 +3,76 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
+	"strings"
 
-	"github.com/William-LP/go_training/pkg/classes"
 	"github.com/William-LP/go_training/pkg/player"
 
 	"github.com/common-nighthawk/go-figure"
 )
 
+func getInput(textToPrint string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(textToPrint)
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.TrimSuffix(userInput, "\r\n")
+
+	return userInput
+}
+
+func mainMenu(p player.Player) {
+	fmt.Println()
+	fmt.Println("Start a game (1)")
+	fmt.Println("Open the shop (2)")
+	fmt.Println("Exit the game (3)")
+	fmt.Println()
+	switch getInput("Choice : ") {
+	case "1":
+		startGame()
+	case "2":
+		openShop(p)
+	case "3":
+		exitGame()
+	default:
+		fmt.Println("Error, please make a correct choice")
+		mainMenu(p)
+	}
+}
+
+func exitGame() {
+	fmt.Println("Thanks for playing, adios !")
+	os.Exit(0)
+}
+
+func startGame() {
+	fmt.Println("Lets start a game !")
+}
+
+func openShop(p player.Player) {
+	fmt.Println()
+	myFigure := figure.NewFigure("Shop", "rectangles", true)
+	myFigure.Print()
+	fmt.Println()
+	fmt.Println("Welcome to the shop", p.Name, "!")
+	fmt.Println("You've got", p.Gold, "gold(s) available.")
+
+}
+
 func main() {
 	myFigure := figure.NewFigure("DOS Fight", "rectangles", true)
 	myFigure.Print()
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Player's Name: ")
-	playerName, _ := reader.ReadString('\n')
+	playerName := getInput("Player's Name: ")
+
 	p := player.NewPlayer(playerName)
-	log.Println(p.Gold)
 
-	h := classes.CreateClass("Mage")
+	mainMenu(p)
+	/*
+		h := classes.CreateClass("Mage")
 
-	log.Println("Health Point : ", h.GetHealthPoint())
-	log.Println("Magic Point: ", h.GetMagicPoint())
-	log.Println("Strength : ", h.GetStrength())
-	log.Println("Intelect: ", h.GetIntelect())
-
+		fmt.Println("Health Point : ", h.GetHealthPoint())
+		fmt.Println("Magic Point: ", h.GetMagicPoint())
+		fmt.Println("Strength : ", h.GetStrength())
+		fmt.Println("Intelect: ", h.GetIntelect())
+	*/
 }
