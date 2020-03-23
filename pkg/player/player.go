0690f711a7d815db77e5980e57115030a8063961
Will <william.lepommelet@gmail.com>
2020-03-23 17:16:29 -0300

    add some player stats bonus

--- pkg/player/player.go
+++ pkg/player/player.go
@@ -3,14 +3,22 @@ package player
 // Player ;
 type Player struct {
 	Name string
-	Gold int
+	Gold,
+	BonusHealthPoint,
+	BonusMagicPoint,
+	BonusStrength,
+	BonusIntelect int
 }
 
 // NewPlayer ;
 func NewPlayer(name string) Player {
 	np := Player{
-		Name: name,
-		Gold: 50,
+		Name:             name,
+		Gold:             50,
+		BonusHealthPoint: 0,
+		BonusMagicPoint:  0,
+		BonusStrength:    0,
+		BonusIntelect:    0,
 	}
 	return np
 }
